fc1c8974ad635a0664a166308f6285210dacad58
unknown <tiagoarodrigues.dev@gmail.com>
2024-08-05 20:11:43 -0300

    feat: add icon app and app loading

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import "./style/global.css";
-import "./style/style.css";
-
-import MyApp from "./Components/MyApp/MyApp";
-
+import React, { useState, useEffect } from 'react';
+import './style/global.css';
+import './style/style.css';
+import MyApp from './Components/MyApp/MyApp';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, []);
 
   return (
     <div className="container">
       <ToastContainer />
 
-      <div className="todo">
+      {loading ? (
+        <div className="text-center mt-5">
+          <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+          <div className="mt-3">
+            <strong>Carregando...</strong>
+          </div>
+        </div>
+      ) : (
+        <div className="todo">
           <MyApp />
-      </div>
-
+        </div>
+      )}
     </div>
   );
 }
